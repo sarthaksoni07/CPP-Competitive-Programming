b96db9fbd8bb3fea761b0cb74f6fdbe42514cab3
sarthaksoni07 <sarthaksoni07@gmail.com>
2025-08-01 23:21:48 +0530

    introduction to c++

---
+++ 1.cpp
@@ -0,0 +1,10 @@
+// intro to cpp
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  
+    cout << "hello world";// this is c out used to take user output
+    return 0;
+}
+// the forward arrow > is used when we want to take some input from the user, the backward arrow < is used when we want to dispplay something on the scren 
