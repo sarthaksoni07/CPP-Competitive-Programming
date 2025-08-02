3703e9e89d4a65139c11b9db442d4f012b852bf8
sarthaksoni07 <sarthaksoni07@gmail.com>
2025-08-02 22:13:16 +0530

    cpp program

---
+++ 2.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int a;
+    char c;
+    float f;
+    double d;
+    bool b;
+    cout << "enter a integer:-";
+    cin >> a;
+    cout << "enter a charecter:-";
+    cin >> c;
+    cout << "enter a floating point:-";
+    cin >> f;
+    cout << "enter a double:-";
+    cin >> d;
+    cout << "enter a bool:-";
+    cin >> b;
+
+    cout << "integer: " << a << endl;
+    cout << "char: " << c << endl;
+    cout << "float: " << f << endl;
+    cout << "double: " << d << endl;
+    cout << "bool: " << b << endl;
+
+    return 0;
+}
