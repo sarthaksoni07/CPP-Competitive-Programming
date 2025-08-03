aa23e3ac9005c25ef3ab9fbb34ea002e768c1729
sarthaksoni07 <sarthaksoni07@gmail.com>
2025-08-03 23:50:29 +0530

    code

---
+++ 3.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(0);
+cout.tie(0);
+
+int x;
+cin>>x;
+while(x--){ // this loop will run until x=0
+
+}
+return 0;
+}
