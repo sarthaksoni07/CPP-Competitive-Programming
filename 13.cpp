9aee843f0b46edb66ba328299796938a4ea70024
sarthaksoni07 <sarthaksoni07@gmail.com>
2025-08-23 16:28:50 +0530

    hollow box printing technique

--- 13.cpp
+++ 13.cpp
@@ -1,20 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
-string star(int i , int j, int r ,int c){
-    return "*";
+
+char star(int i, int j, int r, int c)
+{
+    if (i == 0 || i == r - 1 || j == 0 || j == c - 1)
+    {
+        return '*';
+    }
+    else
+    {
+        return ' ';
+    }
 }
 int main()
 {
-//patter matching techniques in cpp
+    // patter matching techniques in cpp
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
 
- int r,c; 
- cin>>r>>c;
- for(int i=0;i<r;i++){
-    for(int j = 0 ;j<c;j++){
-        cout<<star(i,j,r,c);
+    int r, c, i, j;
+    cin >> r >> c;
+    for (i = 0; i < r; i++)
+    {
+        for (j = 0; j < c; j++)
+        {
+            cout << star(i, j, r, c);
+        }
+        cout << endl;
     }
-    cout<<endl;
- }
-return 0;
+    return 0;
 }
