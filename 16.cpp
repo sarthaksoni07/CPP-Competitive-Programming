44c8b7d47d7cf92e10e682a641372901e8dfe1d7
sarthaksoni07 <sarthaksoni07@gmail.com>
2025-08-26 20:00:00 +0530

    finding an element in the array

--- 16.cpp
+++ 16.cpp
@@ -1,8 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
+// if x exist in an array and what is it's position
+
+void findArray(int arr[], int n, int s){
+  int position = -1;
+    for(int i = 0 ; i <n;i++){
+        if(s == arr[i]){
+            position = i ;
+            break;
+        }
+    }
+    cout<<position<<endl;
+}
  
 int main()
 {
+int arr [] = {1,2,3,4,5,6};
+int n = sizeof(arr)/sizeof(arr[0]);
 
+findArray(arr,n,5);
 return 0;
 }
