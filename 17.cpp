b11774c99e21dd5d74b222cb9078951997ab7bea
sarthaksoni07 <sarthaksoni07@gmail.com>
2025-08-28 20:00:00 +0530

    pointer's good examples

--- 17.cpp
+++ 17.cpp
@@ -7,5 +7,11 @@ int main()
 int x = 5;
 int* ptr = &x;
 cout<<*ptr<<endl; 
+int arr[]={1,2,3,4,5,6,7};
+//these 3 lines will give the same output because this is how pointers works
+cout<<arr[3]<<'\n';
+cout<<3[arr]<<'\n';
+cout<<*(arr+3)<<'\n';
+
 return 0;
 }
