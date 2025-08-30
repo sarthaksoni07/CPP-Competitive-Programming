554951ed57356a1abe2f3fd975b00951f5e195e7
sarthaksoni07 <sarthaksoni07@gmail.com>
2025-08-30 20:00:00 +0530

    strings

---
+++ 19.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+//string
+string a; //initialize blank
+string b = "sarthak";//initialize
+string c(b);//copies the string into c
+string d = ("sarthak1");//in bracket
+string e = d; // copy again 
+ 
+cout<<a<<'\n'<<b<<'\n'<<c<<'\n'<<d<<'\n'<<e<<'\n';
+return 0;
+}
