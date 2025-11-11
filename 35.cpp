21c167f30e8c335fdacf4361d37e922313bee069
Sarthak <sarthaksoni07@gmail.com>
2025-11-11 20:00:00 +0530

    reverse words

--- 35.cpp
+++ 35.cpp
@@ -1,21 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
+
 int main()
 {
     string s;
-    cin>>s;
-    int start= 0;
-    int end = 0;
-    int n = s.length();
-    for(int i= 0 ; i < n ;i++){
-        while(s[i]!=' '){
-            end++;
-        }
-        if(s[i]==' '){
-            reverse(s.begin()+start, s.begin()+end+1);
-            start = end+1;
-        }
+    while (cin >> s)
+    {
+        reverse(s.begin(), s.end());
+        cout << s << " ";
     }
+    // cin, only run if there is no space, when there is a space is stops. we have used while loop because it run differenct times for different strings
+
     return 0;
 }
