9a6cf6790353a1544580aeb9c7543704b6ccd502
Sarthak <sarthaksoni07@gmail.com>
2025-11-11 20:00:00 +0530

    reverse words

--- 35.cpp
+++ 35.cpp
@@ -1,9 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
     string s;
     cin>>s;
     int start= 0;
     int end = 0;
     int n = s.length();
-    for(int i= 0 ; i < n ;i ++){
+    for(int i= 0 ; i < n ;i++){
         while(s[i]!=' '){
             end++;
         }
@@ -12,3 +17,5 @@
             start = end+1;
         }
     }
+    return 0;
+}
