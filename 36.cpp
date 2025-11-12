e10316c9f997da3725e514c096db9224b18fdeff
Sarthak <sarthaksoni07@gmail.com>
2025-11-12 20:00:00 +0530

    count alphabets in a string

--- 36.cpp
+++ 36.cpp
@@ -1,23 +1,45 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-void solution() {
-    string s; cin >> s;
-    int n = s.size();
-    int arr[26] = {0}; // initializes whole array with zero
-    for (int i = 0; i < n; i++) {
+// void solution() {
+//     string s; cin >> s;
+//     int n = s.size();
+//     int arr[26] = {0}; // initializes whole array with zero
+//     for (int i = 0; i < n; i++) {
+//         arr[s[i] - 'a']++;
+//     }
+//     for (int i = 0; i < 26; i++) {
+//         if (arr[i]) {
+//             cout << (char)(i + 'a') << " : " << arr[i] << endl;
+//         }
+//     }
+// }
+
+void solve()
+{
+    string s;
+    cin >> s;
+    int arr[26] = {0};
+    int l = s.length();
+    for (int i = 0; i < l; i++)
+    {
         arr[s[i] - 'a']++;
     }
-    for (int i = 0; i < 26; i++) {
-        if (arr[i]) {
-            cout << (char)(i + 'a') << " : " << arr[i] << endl;
+    for (int j = 0; j < 26; j++)
+    {
+        if (arr[j])
+        {
+            cout << (char)(j + 'a') << " : " << arr[j] << endl;
         }
     }
 }
 
-int main() {
+int main()
+{
     ios_base::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
-    solution();
+    cin.tie(0);
+    cout.tie(0);
+    // solution();
+    solve();
     return 0;
 }
