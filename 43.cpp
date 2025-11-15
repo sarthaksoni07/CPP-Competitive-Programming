1da422451fc12f89f2ef21775556635915a22778
Sarthak <sarthaksoni07@gmail.com>
2025-11-15 20:00:00 +0530

    container with most water

--- 43.cpp
+++ 43.cpp
@@ -1,60 +1,21 @@
-#include <bits/stdc++.h>
-using namespace std;
-void solve()
-{
-    class Solution
-    {
-    public:
-        int maxArea(vector<int> &height)
-        {
-            int answer = 0;
-            for (int i = 0; i < height.size(); i++)
-            {
-                for (int j = 0; j < height.size(); j++)
-                {
-                    int diff;
-                    int result = 0;
-                    if (i == j)
-                    {
-                        continue;
-                    }
-                    if (i > j)
-                    {
-                        diff = i - j;
-                    }
-                    else if (j > i)
-                    {
-                        diff = j - i;
-                    }
-                    if (height[i] > height[j])
-                    {
-                        result = height[j] * diff;
-                    }
-                    else if (height[j] > height[i])
-                    {
-                        result = height[i] * diff;
-                    }
-                    else if (height[i] == height[j])
-                    {
-                        result = height[i] * diff;
-                    }
-                    if (result >= answer)
-                    {
-                        answer = result;
-                    }
-                }
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int answer = 0;
+        int result;
+        int left = 0;
+        int right = height.size() - 1;
+        while (left < right) {
+            result = (right - left) * min(height[left], height[right]);
+            if (result > answer) {
+                answer = result;
             }
-            return answer;
+            if (height[left] > height[right]) {
+                right--;
+            } else {
+                left++;
+            }
         }
-    };
-}
-int main()
-{
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-
-    solve();
-
-    return 0;
-}
+        return answer;
+    }
+};
