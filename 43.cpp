4880b5fbac0e5f0327adadaa5569c4a15a9a8354
Sarthak <sarthaksoni07@gmail.com>
2025-11-15 20:00:00 +0530

    container with most water

--- 43.cpp
+++ 43.cpp
@@ -1,21 +1,43 @@
-class Solution {
-public:
-    int maxArea(vector<int>& height) {
-        int answer = 0;
-        int result;
-        int left = 0;
-        int right = height.size() - 1;
-        while (left < right) {
-            result = (right - left) * min(height[left], height[right]);
-            if (result > answer) {
-                answer = result;
+#include <bits/stdc++.h>
+using namespace std;
+void sovle()
+{
+    class Solution
+    {
+    public:
+        int maxArea(vector<int> &height)
+        {
+            int answer = 0;
+            int result;
+            int left = 0;
+            int right = height.size() - 1;
+            while (left < right)
+            {
+                result = (right - left) * min(height[left], height[right]);
+                if (result > answer)
+                {
+                    answer = result;
+                }
+                if (height[left] > height[right])
+                {
+                    right--;
+                }
+                else
+                {
+                    left++;
+                }
             }
-            if (height[left] > height[right]) {
-                right--;
-            } else {
-                left++;
-            }
+            return answer;
         }
-        return answer;
-    }
-};
+    };
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    sovle();
+
+    return 0;
+}
