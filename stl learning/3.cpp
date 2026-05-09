9cfe240c0631f730025253de5b54b8baed767b7f
sarthaksoni07 <sarthaksoni07@gmail.com>
2026-05-09 23:27:29 +0530

    vectors

--- stl learning/3.cpp
+++ stl learning/3.cpp
@@ -9,9 +9,19 @@ void printVec(vector<pair<int,int>> &v)
     }
     cout <<endl;
 }
+void printVec(vector<pair<int,string>> &v) 
+{ 
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i].first<<" "<< v[i].second << endl; 
+    }
+    cout <<endl;
+}
 int main()
 {
     vector<pair<int,int>> v;// we can use pair inside of vectors,and other types of containers inside the vector. 
+    vector<pair<int,string>> v1 = {{1,"sarthak"}, {2,"sparsh"}};
+    printVec(v1);
     v.push_back({1,2});
     v.push_back({3,4});
     v.push_back({5,6});
