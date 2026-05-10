b741db15456a8df8d9e683f36c831370aad08f45
sarthaksoni07 <sarthaksoni07@gmail.com>
2026-05-10 12:55:12 +0530

    array of vectors

---
+++ stl learning/4.cpp
@@ -0,0 +1,38 @@
+// array of vectors, and vector of vectors
+#include <bits/stdc++.h>
+using namespace std;
+void printVec(vector<int> &v)
+{
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i]<< " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    int N;
+    cin >> N;
+    vector<int> v[N];
+    for (int i = 0; i < N; i++)
+    {
+        int n;
+        cin >> n;
+        for (int j = 0; j < n; j++)
+        {
+            int x;
+            cin >> x;
+            v[i].push_back(x);
+        }
+    }
+    for (int i = 0; i < N; i++)
+    {
+        printVec(v[i]);
+    }
+    return 0;
+}
+// this is an array of vector
