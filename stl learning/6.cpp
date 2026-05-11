772bd05536dc2dce0594b9c5c9a72e5d9a453106
sarthaksoni07 <sarthaksoni07@gmail.com>
2026-05-11 13:09:27 +0530

    ignore input and output files

---
+++ stl learning/6.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    //iterators in stl 
+    int x;
+    cin>>x;
+    cout<<x;
+     
+    return 0;
+}
