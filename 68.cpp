e91cdd958bf170a89062fa42b0af763bcd389591
Sarthak <sarthaksoni07@gmail.com>
2026-05-30 11:28:10 +0530

    max consecutive ones

---
+++ 68.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int count= 0; 
+        int max= 0;
+        for(int i = 0 ; i < nums.size();i++){
+            if(nums[i]==1){
+                count++;
+            }
+            else{
+                count=0;
+            }
+            if(count>max){
+                max=count;
+            }
+        }
+        return max;
+    }
+};
+// max consecutive ones 
+// quest array 1 , question 3
